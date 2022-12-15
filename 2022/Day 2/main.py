d5018e3dd51f75caf24d590a1bfec8b478814df0
Olav Ausland Onstad <olavao@uia.no>
2022-12-15 23:31:02 +0100

    Solved: Day [2.1, 2.2]

--- 2022/Day 2/main.py
+++ 2022/Day 2/main.py
@@ -1,7 +1,56 @@
-def main():
+from copy import deepcopy
+
+def part_one() -> int:
+    code: dict = {
+        'A': ('Rock', 'Paper'), 'B': ('Paper', 'Scissor'), 'C': ('Scissor', 'Rock'),
+        'X': ('Rock', 'Paper'), 'Y': ('Paper', 'Scissor'), 'Z': ('Scissor', 'Rock')
+    }
+
+    code_points: dict = {'Rock': 1, 'Paper': 2, 'Scissor': 3}
+
+    score: int = 0
+
     with open('./puzzle.txt', 'r') as file:
         for line in file:
-            pass
+            enemy, player = line.split()
+
+            if code[enemy][0] == code[player][0]:
+                score += (3 + code_points[code[player][0]])
+            elif code[enemy][1] == code[player][0]:
+                score += (6 + code_points[code[player][0]])
+            else:
+                score += (0 + code_points[code[player][0]])
+    return score
+
+
+def part_two() -> int:
+    code: dict = {
+        'A': ('Rock', 'Paper'), 'B': ('Paper', 'Scissor'), 'C': ('Scissor', 'Rock'),
+        'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'
+    }
+
+    code_points: dict = {'Rock': 1, 'Paper': 2, 'Scissor': 3}
+
+    score: int = 0
+
+    with open('./puzzle.txt', 'r') as file:
+        for line in file:
+            enemy, player = line.split()
+
+            if code[player] == 'Lose':
+                temp = list(deepcopy(code_points))
+                [temp.remove(x) for x in list(code[enemy])]
+                score += (0 + code_points[temp[0]])
+            elif code[player] == 'Draw':
+                score += (3 + code_points[code[enemy][0]])
+            elif code[player] == 'Win':
+                score += (6 + code_points[code[enemy][1]])
+    return score
+
+
+def main():
+    print(f'Part One: {part_one()}')
+    print(f'Part Two: {part_two()}')
 
 
 if __name__ == '__main__':
