0c189813a973c5409d9f56a78158679a2687b212
Olav Ausland Onstad <olavao@uia.no>
2022-12-16 16:09:19 +0100

    Solved: Day[5.2]

--- 2022/Day 5/main.py
+++ 2022/Day 5/main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import numpy as np
+from typing import TextIO, List
 
 
 def load_puzzle() -> list:
@@ -9,31 +10,36 @@ def load_puzzle() -> list:
         stacks = [[] for _ in range(9)]
 
         for i, line in enumerate(file):
+
             if str(line[1]).isnumeric():
                 next(file)
                 break
             for char in range(1, len(line), 4):
+                if len(re.findall('[a-zA-Z]', line)) > len(stacks):
+                    stacks = [[] for i in range(len(re.findall('[a-zA-Z]', line)))]
                 if bool(re.search(pattern, line[char])):
                     stacks[(char // 4)].insert(0, line[char])
     return stacks
+
+
+def skip_initial_stack(file: TextIO):
+    for line in file:
+        if str(line[1]).isnumeric():
+            next(file)
+            break
 
 
 def part_one():
     result: str = ''
     with open(f'{os.path.dirname(__file__)}\\puzzle.txt', 'r') as file:
         stacks = load_puzzle()
-
-        for line in file:
-            if str(line[1]).isnumeric():
-                next(file)
-                break
+        skip_initial_stack(file)
 
         for line in file:
             command = np.asarray(re.findall('[0-9]+', line), dtype=int)
             for i in range(command[0]):
                 crate = stacks[command[1] - 1].pop()
                 stacks[command[2] - 1].append(crate)
-
         for stack in stacks:
             if len(stack) > 0:
                 result += stack.pop()
@@ -44,26 +50,24 @@ def part_one():
 def part_two():
     result: str = ''
     with open(f'{os.path.dirname(__file__)}\\puzzle.txt', 'r') as file:
-        stacks = load_puzzle()
-
-        for line in file:
-            if str(line[1]).isnumeric():
-                next(file)
-                break
+        stacks: List[List] = load_puzzle()
+        skip_initial_stack(file)
 
         for line in file:
             command = np.asarray(re.findall('[0-9]+', line), dtype=int)
-            print(command)
-            crates = stacks[command[1]-1][-command[0]:]
-            print(command[0], crates)
+            crates = []
+
+            for i in range(command[0]):
+                crates.append(stacks[command[1]-1].pop())
+
             for crate in reversed(crates):
                 stacks[command[2] - 1].append(crate)
-            print(stacks[command[2] - 1])
 
         for stack in stacks:
             if len(stack) > 0:
                 result += stack.pop()
     return result
+
 
 def main():
     print(
