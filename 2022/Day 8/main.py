d05f42a331643742f40d3542da4df2747e74e173
Olav Ausland Onstad <olavao@uia.no>
2022-12-16 21:17:01 +0100

    Added: Day 8 folder

---
+++ 2022/Day 8/main.py
@@ -0,0 +1,20 @@
+import os
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+def main():
+    print(
+        f'\N{File Folder}{os.path.dirname(__file__).split(chr(92))[-1]}:\n'
+        f'\t\N{pushpin}{part_one()}\n'
+        f'\t\N{pushpin}{part_two()}'
+    )
+
+
+if __name__ == '__main__':
+    main()
